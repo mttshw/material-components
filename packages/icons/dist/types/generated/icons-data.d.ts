edb1cd759543e7fee3e137643a026b140c0a86e4
Matt Shaw <matt@mttshw.com>
2026-06-09 10:29:15 +0100

    icon build

---
+++ packages/icons/dist/types/generated/icons-data.d.ts
@@ -0,0 +1,8 @@
+export interface IconMeta {
+    name: string;
+    categories: string[];
+    tags: string[];
+}
+export declare const ICONS: Record<string, string>;
+export declare const ICONS_META: IconMeta[];
+//# sourceMappingURL=icons-data.d.ts.map
